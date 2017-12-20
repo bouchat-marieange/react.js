7bcad609261b748b141e02ef9821622a27a84c07
Bouchat Marie-Ange <bouchat.marieange@gmail.com>
2017-12-20 09:32:43 +0100

    Recommence toutes les étapes de la création et installation seconde appli

--- seconde_app/component/Welcome.js
+++ seconde_app/component/Welcome.js
@@ -1,17 +1,8 @@
-import React from 'react'; // On indique React que notre component est un component. Quand on ne met pas ./ avant, par defaut, l'ordinateur va chercher par defaut dans le dossier node_modules que nodeJS à installer avec la commande react-create_app et un de ces dossier s'appelle React, c'est donc là qu'il va le charger sans que l'on ai besoin de lui indiquer tout le chemin d'accès.
-
+// On indique React que notre component est un component. Quand on ne met pas ./ avant, par defaut, l'ordinateur va chercher par defaut dans le dossier node_modules que nodeJS à installer avec la commande react-create_app et un de ces dossier s'appelle React, c'est donc là qu'il va le charger sans que l'on ai besoin de lui indiquer tout le chemin d'accès.
+import React from 'react';
 
 export default class Welcome extends React.Component {
     render() {
       return <h1>Hello</h1>;
     }
 }
-
-// on peut également indiquer le code qui précéde class Welcome et l'export sous cette forme
-    // class Welcome extends React.Component {
-    //   render() {
-    //     return <h1>Hello</h1>;
-    //   }
-    // }
-    //
-    // export default Welcome;
--- seconde_app/src/App.js
+++ seconde_app/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Welcome from './component/Welcome'; // On importe notre class "Welcome" de ./ (dans le même dossier) - React comprend que c'est du JS (inutile de d'indiquer Welcome.js, par contre si c'est un autre langage, il faut lui préciser)
+import Welcome from './component/Welcome';
 
 class App extends Component {
   render() {
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Bienvenue mon Application</h1>
+          <h1 className="App-title">Bienvenue dans mon application</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
